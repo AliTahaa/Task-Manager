569057071b31349a928e3480dfb86bdb283cc09b
AliTahaa <ali.mohamed.taha.h@gmail.com>
2025-01-23 00:02:39 +0200

    add task creation feature

--- app.py
+++ app.py
@@ -30,6 +30,30 @@ class Task(db.Model):
 def index():
     return redirect(url_for('login'))
 
+@app.route('/add', methods=['POST'])
+def add_task():
+    title = request.form['title']
+    new_task = Task(title=title, user_id=session['user_id'])
+    db.session.add(new_task)  # Add task to the session
+    db.session.commit()  # Commit the transaction
+    return redirect(url_for('dashboard'))  # Redirect to the index page
+
+
+@app.route('/update', methods=['POST'])
+def update_tasks():
+    completed_task_ids = request.form.getlist('completed_tasks')  # Get the selected tasks
+    user_id = session['user_id']  # Get the current user's ID
+    tasks = Task.query.filter_by(user_id=user_id).all()  # Filter tasks by the current user
+
+    for task in tasks:
+        if str(task.id) in completed_task_ids:
+            task.is_completed = True
+        else:
+            task.is_completed = False
+
+    db.session.commit()  # Commit the updates
+    return redirect(url_for('dashboard'))  # Redirect to the index page
+
 
 @app.route('/delete/<int:id>')
 def delete_task(id):
@@ -73,6 +97,14 @@ def login():
             return "Invalid email or password!", 400
 
     return render_template('login.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    tasks = Task.query.filter_by(user_id=session['user_id']).all()
+    return render_template('index.html', tasks=tasks)
 
 
 @app.route('/logout')
